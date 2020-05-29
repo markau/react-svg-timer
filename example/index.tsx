005e0426c4aeab35087096a501056204f70c5484
Mark Andrews <20metresbelow@gmail.com>
2020-05-29 11:08:05 +0930

    Updated example with demo functionality

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,220 @@
+// import 'react-app-polyfill/ie11';
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+// import { ReactSvgTimer } from '../.';
+
+// const App = (props) => {
+//   return (
+//     <div style={{ width: 400 }}>
+//       <ReactSvgTimer {...props} />
+//     </div>
+//   );
+// };
+
+// ReactDOM.render(<App timerCount={20} />, document.getElementById('root'));
+
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { useState, useEffect } from 'react'
+import reactCSS from 'reactcss'
+import { SketchPicker } from 'react-color'
 import { ReactSvgTimer } from '../.';
 
-const App = (props) => {
-  return (
-    <div style={{ width: 400 }}>
-      <ReactSvgTimer {...props} />
-    </div>
-  );
-};
+const App = () => {
+  // State variables
+  let [resetRequested, setResetRequested] = useState(false)
+  let [timerisComplete, setTimerisComplete] = useState(false)
+  let [displayCountdown, setDisplayCountdown] = useState(true)
+  let [showMilliseconds, setShowMilliseconds] = useState(false)
+  let [displayCountdownColorPicker, setDisplayCountdownColorPicker] = useState(false)
+  let [displayInnerColorPicker, setDisplayInnerColorPicker] = useState(false)
+  let [displayOuterColorPicker, setDisplayOuterColorPicker] = useState(false)
+  let [countdownColor, setCountdownColor] = useState({ r: '65', g: '182', b: '224', a: '1' })
+  let [innerColor, setInnerColor] = useState({ r: '255', g: '255', b: '255', a: '1' })
+  let [outerColor, setOuterColor] = useState({ r: '40', g: '40', b: '40', a: '1' })
 
-ReactDOM.render(<App timerCount={20} />, document.getElementById('root'));
+  useEffect(() => {
+    if (timerisComplete) console.log('timer complete')
+  })
+
+  const handleCountdownColorClick = () => {
+    setDisplayCountdownColorPicker(!displayCountdownColorPicker)
+  }
+  const handleCountdownColorClose = () => {
+    setDisplayCountdownColorPicker(false)
+  }
+  const handleCountdownColorChange = color => {
+    setCountdownColor(color.rgb)
+  }
+  const handleInnerColorClick = () => {
+    setDisplayInnerColorPicker(!displayInnerColorPicker)
+  }
+  const handleInnerColorClose = () => {
+    setDisplayInnerColorPicker(false)
+  }
+  const handleInnerColorChange = color => {
+    setInnerColor(color.rgb)
+  }
+  const handleOuterColorClick = () => {
+    setDisplayOuterColorPicker(!displayOuterColorPicker)
+  }
+  const handleOuterColorClose = () => {
+    setDisplayOuterColorPicker(false)
+  }
+  const handleOuterColorChange = color => {
+    setOuterColor(color.rgb)
+  }
+  const onComplete = status => {
+    setTimerisComplete(status)
+  }
+  const onReset = () => {
+    setResetRequested(false)
+  }
+  const timerValue = value => {
+    if (showMilliseconds) console.log(value)
+  }
+  const onResetRequest = () => {
+    setResetRequested(true)
+  }
+  const optionTimerText = () => {
+    setDisplayCountdown(displayCountdown)
+  }
+  const optionLogCount = () => {
+    setShowMilliseconds(showMilliseconds)
+  }
+
+  const styles = reactCSS({
+    default: {
+      countdownColor: {
+        width: '36px',
+        height: '14px',
+        borderRadius: '2px',
+        background: `rgba(${countdownColor.r}, ${countdownColor.g}, ${countdownColor.b}, ${countdownColor.a})`
+      },
+      outerColor: {
+        width: '36px',
+        height: '14px',
+        borderRadius: '2px',
+        background: `rgba(${outerColor.r}, ${outerColor.g}, ${outerColor.b}, ${outerColor.a})`
+      },
+      innerColor: {
+        width: '36px',
+        height: '14px',
+        borderRadius: '2px',
+        background: `rgba(${innerColor.r}, ${innerColor.g}, ${innerColor.b}, ${innerColor.a})`
+      },
+      swatch: {
+        padding: '5px',
+        background: '#fff',
+        borderRadius: '1px',
+        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+        display: 'inline-block',
+        cursor: 'pointer',
+        marginRight: '10px'
+      },
+      popover: {
+        position: 'absolute',
+        zIndex: '2'
+      },
+      cover: {
+        position: 'fixed',
+        top: '0px',
+        right: '0px',
+        bottom: '0px',
+        left: '0px'
+      },
+      formElement: {
+        padding: 10,
+        textAlign: 'left'
+      },
+      resetButton: {
+        padding: 10,
+        width: '90%'
+      },
+      heading: {
+        padding: 5,
+        paddingLeft: 15,
+        fontSize: 22
+      }
+    }
+  })
+
+  return (
+    <div className="columnsContainer">
+      <div className="timerContainer">
+        <ReactSvgTimer
+          timerCount={6}
+          countdownColor={`rgba(${countdownColor.r}, ${countdownColor.g}, ${countdownColor.b}, ${countdownColor.a})`}
+          innerColor={`rgba(${innerColor.r}, ${innerColor.g}, ${innerColor.b}, ${innerColor.a})`}
+          outerColor={`rgba(${outerColor.r}, ${outerColor.g}, ${outerColor.b}, ${outerColor.a})`}
+          resetTimer={onReset}
+          completeTimer={onComplete}
+          resetTimerRequested={resetRequested}
+          timerDuration={timerValue}
+          displayCountdown={displayCountdown}
+        />
+      </div>
+
+      <div className="controlsContainer">
+        <p style={styles.heading}>Play with some props:</p>
+
+        <div style={styles.formElement}>
+          <button style={styles.resetButton} onClick={onResetRequest}>
+            Reset timer
+          </button>
+        </div>
+
+        <div style={styles.formElement}>
+          <div style={styles.swatch} onClick={handleCountdownColorClick}>
+            <div style={styles.countdownColor} />
+          </div>
+          {displayCountdownColorPicker ? (
+            <div style={styles.popover}>
+              <div style={styles.cover} onClick={handleCountdownColorClose} />
+              <SketchPicker color={countdownColor} onChange={handleCountdownColorChange} />
+            </div>
+          ) : null}
+          Countdown color
+        </div>
+
+        <div style={styles.formElement}>
+          <div style={styles.swatch} onClick={handleOuterColorClick}>
+            <div style={styles.outerColor} />
+          </div>
+          {displayOuterColorPicker ? (
+            <div style={styles.popover}>
+              <div style={styles.cover} onClick={handleOuterColorClose} />
+              <SketchPicker color={outerColor} onChange={handleOuterColorChange} />
+            </div>
+          ) : null}
+          Outer color
+        </div>
+
+        <div style={styles.formElement}>
+          <div style={styles.swatch} onClick={handleInnerColorClick}>
+            <div style={styles.innerColor} />
+          </div>
+          {displayInnerColorPicker ? (
+            <div style={styles.popover}>
+              <div style={styles.cover} onClick={handleInnerColorClose} />
+              <SketchPicker color={innerColor} onChange={handleInnerColorChange} />
+            </div>
+          ) : null}
+          Inner color
+        </div>
+
+        <div style={styles.formElement}>
+          <input defaultChecked={displayCountdown} onClick={optionTimerText} type="checkbox" />
+          Show countdown
+        </div>
+        <div style={styles.formElement}>
+          <input defaultChecked={showMilliseconds} onClick={optionLogCount} type="checkbox" />
+          Log elapsed milliseconds to console
+        </div>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
