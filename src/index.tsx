a4dd361279bfd1bba506e66e94496db6da3a7940
Mark Andrews <20metresbelow@gmail.com>
2020-10-19 08:17:59 +1030

    reverted draw coord

--- src/index.tsx
+++ src/index.tsx
@@ -74,7 +74,7 @@ export const ReactSvgTimer: FC<ITimerProps> = ({
       setDraw(drawCoord(duration * degrees));
     } else {
       completeTimer(true);
-      setDraw(drawCoord(0));
+      setDraw(drawCoord(359.99));
     }
     // Inform the parent component of the current timer duration
     if (timerDuration) timerDuration(duration);
